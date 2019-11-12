6894cc80a328f2afcfb70fcb9b494159b46330ed
nouvelle <nouvellenuts@gmail.com>
2019-11-12 19:15:10 +0900

    add: migrations file

---
+++ server/migrations/20191112185511_add_coffeetime_table.js
@@ -0,0 +1,37 @@
+// yarn run knex migrate:make add_coffeetime_table
+exports.up = function(knex) {
+  // create the 'voca' table with six columns
+  return knex.schema.createTable("coffeetime", t => {
+    t.increments() // auto-incrementing id column
+      .index(); // index this column
+
+    t.string("URL", 300) // maximum length of 300 characters
+      .unique() // add a unique constraint to this column
+      .notNullable() // add a not-null constraint to this column
+      .index(); // index it
+
+    t.integer("date", 15) // maximum length of 15 characters
+      .index(); // index it
+
+    t.string("name", 100) // maximum length of 100 characters
+      .index(); // index it
+
+    t.boolean("isRead");
+
+    t.integer("readDate", 15) // maximum length of 15 characters
+      .index(); // index it
+
+    t.string("thumbnail", 10000); // maximum length of 10000 characters
+
+    t.string("tag", 100); // maximum length of 100 characters
+
+    t.timestamp("created_at")
+      .notNullable()
+      .defaultTo(knex.fn.now()); // default to the current time
+  });
+};
+
+exports.down = function(knex) {
+  // undo this migration by destroying the 'voca' table
+  return knex.schema.dropTable("coffeetime");
+};
